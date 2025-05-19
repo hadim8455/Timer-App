46bb3ac934e36aa413125da6118a8f1a1a5fe5a1
hadim8455 <hadim8455@gmail.com>
2025-05-19 19:52:09 +0500

    Timer App Completed

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-alert("hellow hadi")
+
+function startCountDown(){
+    var count = 10;
+    var timer = document.getElementById("timer")
+    
+    var myInterval = setInterval(function(){
+        timer.innerHTML = count 
+        count = count -1
+
+        if(count < 0){
+            clearInterval(myInterval)
+            timer.innerHTML = "Time's Up"
+        }
+
+    }, 1000)
+
+
+
+}
+
+
